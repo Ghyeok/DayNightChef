25e3861fe151dc738202c05038e087f52f1289e7
Ghyeok <gulww9997@naver.com>
2025-09-24 18:49:44 +0900

    Add: Item에 enum, 필드 추가

--- Assets/Scripts/UI/Inventory/Item.cs
+++ Assets/Scripts/UI/Inventory/Item.cs
@@ -1,10 +1,29 @@
 using System.IO.Enumeration;
 using UnityEngine;
 
+public enum ItemType
+{
+    Animal,
+    Fish,
+    Gather,
+    Grocery,
+
+}
+
+public enum MapType
+{
+    Warm,
+    Hot,
+    Cold,
+    MaxCount,
+}
+
 [CreateAssetMenu(fileName = "NewItem", menuName = "Item")]
 public class Item : ScriptableObject
 {
     public int item_number; // 아이템 번호
+    public ItemType itemType; // 아이템 타입
+    public MapType itemMapType; // 아이템 맵 타입
     public string item_name; // 아이템 이름
     public Sprite item_image; // 아이템 이미지
     public float item_weight; // 아이템 무게
