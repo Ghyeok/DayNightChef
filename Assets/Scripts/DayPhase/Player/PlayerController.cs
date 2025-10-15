e94fe0a9dacab8fdebe85ea4b06ab396d6d5ce26
Ghyeok <gulww9997@naver.com>
2025-10-16 00:17:39 +0900

    Feat: 2D 탑다운 플레이어

--- Assets/Scripts/DayPhase/Player/PlayerController.cs
+++ Assets/Scripts/DayPhase/Player/PlayerController.cs
@@ -1,66 +1,102 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    public VariableJoystick joystick;
+    [Header("Input")]
+    public VariableJoystick joystick; // 없으면 키보드로 폴백
 
-    private CharacterController cc;
+    private Rigidbody2D rb;
     private Animator anim;
 
-    private float verticalVelocity;
+    // 캐시된 입력 벡터(정규화)
+    private Vector2 inputDir = Vector2.zero;
+
+    // 애니메이터 파라미터 유무 캐시
+    private int hashIsWalk, hashIsAttack, hashSpeed, hashMoveX, hashMoveY;
+    private bool hasIsWalk, hasIsAttack, hasSpeed, hasMoveX, hasMoveY;
 
     private void Awake()
     {
-
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        cc = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        // 2D 탑다운 기본 세팅
+        rb.gravityScale = 0f;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        // 파라미터 해시 & 존재 여부 캐시
+        hashIsWalk = Animator.StringToHash("isWalk");
+        hashIsAttack = Animator.StringToHash("isAttack");
+        hashSpeed = Animator.StringToHash("Speed");
+        hashMoveX = Animator.StringToHash("MoveX");
+        hashMoveY = Animator.StringToHash("MoveY");
+
+        hasIsWalk = HasParam(anim, hashIsWalk);
+        hasIsAttack = HasParam(anim, hashIsAttack);
+        hasSpeed = HasParam(anim, hashSpeed);
+        hasMoveX = HasParam(anim, hashMoveX);
+        hasMoveY = HasParam(anim, hashMoveY);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Move();
-        TestAttack();
+        ReadInput();
+        UpdateAnimator();
+        HandleAttack();
     }
 
-    private void Move()
+    private void FixedUpdate()
     {
-        float h = joystick.Horizontal;
-        float v = joystick.Vertical;
-        Vector3 dir = new Vector3(h, 0, v).normalized;
+        float speed = GetMoveSpeed();
+        Vector2 next = rb.position + inputDir * speed * Time.fixedDeltaTime;
+        rb.MovePosition(next);
+    }
 
-        if(cc.isGrounded && verticalVelocity < 0)
+    private void ReadInput()
+    {
+        if (joystick != null)
         {
-            verticalVelocity = -1f;
+            float h = joystick.Horizontal;
+            float v = joystick.Vertical;
+            inputDir = new Vector2(h, v).normalized;
         }
-        else
+        else // 조이스틱 없으면 키보드로
         {
-            verticalVelocity += Physics.gravity.y * Time.deltaTime;
-        }
-
-        Vector3 moveVector = dir * DayPhasePlayerManager.Instance.playerMoveSpeed;
-        moveVector.y = verticalVelocity;
-        cc.Move(moveVector * Time.deltaTime);
-
-        if (dir != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
-            anim.SetBool("isWalk", true);
-        }
-        else if (dir.magnitude < 0.01f)
-        {
-            anim.SetBool("isWalk", false);
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            inputDir = new Vector2(h, v).normalized;
         }
     }
 
-    private void TestAttack()
+    private float GetMoveSpeed()
     {
-        if (Input.GetKey(KeyCode.E)) anim.SetBool("isAttack", true);
-        else anim.SetBool("isAttack", false);
+        // DayPhasePlayerManager를 우선 사용, 없거나 아직 초기화 전이면 fallback
+        var mgr = DayPhasePlayerManager.Instance;
+        return mgr.playerMoveSpeed;
+    }
+
+    private void UpdateAnimator()
+    {
+        float speedVal = inputDir.magnitude * GetMoveSpeed();
+
+        if (hasIsWalk) anim.SetBool(hashIsWalk, speedVal > 0.01f);
+        if (hasSpeed) anim.SetFloat(hashSpeed, speedVal);
+        if (hasMoveX) anim.SetFloat(hashMoveX, inputDir.x);
+        if (hasMoveY) anim.SetFloat(hashMoveY, inputDir.y);
+    }
+
+    private void HandleAttack()
+    {
+        bool attacking = Input.GetKey(KeyCode.E);
+        if (hasIsAttack) anim.SetBool(hashIsAttack, attacking);
+    }
+
+    private bool HasParam(Animator a, int hash)
+    {
+        foreach (var p in a.parameters)
+            if (p.nameHash == hash) return true;
+        return false;
     }
 }
