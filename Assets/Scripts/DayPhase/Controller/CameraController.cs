b1bc88c3d68553daaa0656994ddae7d23eb428e8
Ghyeok <gulww9997@naver.com>
2025-10-16 00:14:18 +0900

    Feat: 탑다운 픽셀 퍼펙트 카메라

--- Assets/Scripts/DayPhase/Controller/CameraController.cs
+++ Assets/Scripts/DayPhase/Controller/CameraController.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    private Vector3 offset = new Vector3(0f, 7f, -6f);
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = target.position + offset;
+        transform.position = offset + target.position;
     }
 }
